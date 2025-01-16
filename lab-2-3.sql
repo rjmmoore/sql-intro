38cda9b55be3eebfbe9b835865e62366dcd97995
rjmmoore Moore <rob.moore@kellogg.northwestern.edu>
2025-01-16 02:42:05 +0000

    completed lab 2 & 3

--- lab-2-3.sql
+++ lab-2-3.sql
@@ -1,5 +1,7 @@
 -- What was the last team (and year) to play at U.S. Cellular Field?
 
+select year, name, park from teams where park = "U.S. Cellular Field" order by year Desc limit 1;
+
 -- Expected result:
 --
 -- +------+-------------------+
